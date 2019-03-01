39f092e6653d8f588acb4f78b1d1f9f67640e6f3
mihirparshy <mihir.parshy@hotmail.com>
2019-03-01 15:53:19 -0500

    read csv and print line

---
+++ main.py
@@ -0,0 +1,7 @@
+import csv
+with open('Character Counter Spreadsheet - Sheet1.csv') as csvfile:
+    readRange = (2, 30) # inclusive range of rows to read
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row_num, row in enumerate(readCSV):
+        if row_num >= readRange[0] and row_num <= readRange[1]:
+            print(row[1:])
