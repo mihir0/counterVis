6d253362483713a708c570883a5adbb67338f348
mihirparshy <mihir.parshy@hotmail.com>
2019-03-01 15:56:31 -0500

    main method

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import csv
-with open('Character Counter Spreadsheet - Sheet1.csv') as csvfile:
-    readRange = (2, 30) # inclusive range of rows to read
-    readCSV = csv.reader(csvfile, delimiter=',')
-    for row_num, row in enumerate(readCSV):
-        if row_num >= readRange[0] and row_num <= readRange[1]:
-            print(row[1:])
+
+if __name__ == "__main__": # main method
+    with open('Character Counter Spreadsheet - Sheet1.csv') as csvfile:
+        readRange = (2, 30) # inclusive range of rows to read
+        readCSV = csv.reader(csvfile, delimiter=',')
+        for row_num, row in enumerate(readCSV):
+            if row_num >= readRange[0] and row_num <= readRange[1]:
+                print(row[1:])
