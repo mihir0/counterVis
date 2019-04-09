e691c80337240cd7ced7df222896c832d8c0303a
isabelle20011 <isabelle.turnes@hotmail.com>
2019-04-09 12:23:40 -0400

    Delete unnecessary things
    deleted graph class

--- main.py
+++ main.py
@@ -1,29 +1,8 @@
 import csv
-from graph import *
 from game import *
 from Hero import *
 
 if __name__ == "__main__":  # main method
-    """graph = Graph()
-    with open('Character Counter Spreadsheet - Sheet1.csv') as csvfile:
-        topRowPosition = 0 # position of header row
-        readRange = (2, 30) # inclusive range of rows to read
-        readCSV = csv.reader(csvfile, delimiter =',')
-        topRow = None
-        for row_num, row in enumerate(readCSV):
-            if row_num == topRowPosition:
-                topRow = row[2:]
-            if row_num >= readRange[0] and row_num <= readRange[1]:
-                row = row[1:] #trim row
-                node_a = Node(row[0])
-                for item_position, item in enumerate(row[1:]):
-                    if item == '1' or item == '2' or item == '3':
-                        # create the edge
-                        node_b = Node(topRow[item_position])
-                        edge = Edge(node_a, node_b, item)
-                        graph.addEdge(edge)
-    #print (graph)"""
-
     with open('Character Counter Spreadsheet - Sheet1.csv') as csvfile:
         # creating list of heroes with counters
 
