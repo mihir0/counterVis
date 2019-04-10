fe38d29549a854359d625d4b7a02be9c44d4dbc3
mihirparshy <mihir.parshy@hotmail.com>
2019-04-10 11:55:42 -0400

    comments

--- main/main.js
+++ main/main.js
@@ -2,7 +2,9 @@
 window.onload = buildHerosList();
 var teamOneSelection = '';
 
-
+/*
+This function generates the HTML list that contains all the hero names contained in arr. This list is made up of clickable buttons with a callback function.
+*/
 function buildHerosList() {
     var arr = ['Ana','Ashe','Bastion','Brigitte','D. Va','Doomfist','Genji','Hanzo','Junkrat','Lucio','Mccree','Mei','Mercy','Moira','Orisa','Pharah','Reaper','Reinhardt','Roadhog','Soldier: 76','Sombra','Symmetra','Torbjorn','Tracer','Widowmaker','Winston','Wrecking Ball','Zarya','Zenyatta'];
     for (var i in arr) {
@@ -13,6 +15,9 @@ function buildHerosList() {
     }
 }
 
+/*
+Called when user types in search box. Filters which heros are visible on list.
+*/
 function filter() {
     var strFilter = $("#myInput").val().toUpperCase();
     // console.log(filter);
@@ -27,6 +32,9 @@ function filter() {
     }
 }
 
+/*
+When hero is clicked, make sure they are selected.
+*/
 function buttonClick(id) {
     var buttons = document.getElementsByTagName("button");
     for (var i = 0; i < buttons.length; i++) {
@@ -36,6 +44,9 @@ function buttonClick(id) {
     teamOneSelection = id;
 }
 
+/*
+Connected to add button. Adds hero to team. Also creates an HTML pill badge with hero name.
+*/
 function addClick(team) {
     console.log(teamOneSelection);
     var element = "<span class='badge badge-pill badge-primary'>" + teamOneSelection + "</span>";
