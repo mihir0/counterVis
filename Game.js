b2ac53d079f6911427653bfa175da41fe711c526
mihirparshy <mihir.parshy@hotmail.com>
2019-04-16 10:10:52 -0400

    fixed Game.js

--- Game.js
+++ Game.js
@@ -1,30 +1,34 @@
 //don't know what needs to be imported
 //also should line 3 be export var or just var
-var Game = __class__ ('Game', [object], {
-    get __init__ () {return __get__ (this, function (self, team1, team2, the_map) {
+export class Game {
+    constructor(team1, team2, the_map) {
         self.team1 = team1;
         self.team2 = team2;
         self.the_map = the_map;
-    });},
-    get team1_select () {return __get__ (this, function (self, hero) {
+    }
+
+    team1_select(hero) {
         if (self.team1.heroes.length <= 6) {
             self.team1.hero_append(hero);
         }
-    });},
-    get team2_select () {return __get__ (this, function (self, hero) {
+    }
+
+    team2_select(hero) {
         if (self.team2.heroes.length <= 6) {
             self.team2.hero_append(hero);
         }
-    });},
-    get calculate_points () {return __get__ (this, function (self) {
-        team1_points = int (0); //not sure how to write int(0) from python
-        team2_points = int (0);
+    }
 
-        team1_points += self.calculate_team1() + self.calculate_map_points1();
-        team2_points += self.calculate_team2() + self.calculate_map_points2();
-    });},
-    get calculate_team1 () {return __get__ (this, function (self) {
-        ans = int (0);
+    // Uncommented because not sure what this method is doing?
+    // get calculate_points() {
+    //     var team1_points = 0;
+    //     var team2_points = 0;
+    //     team1_points += self.calculate_team1() + self.calculate_map_points1();
+    //     team2_points += self.calculate_team2() + self.calculate_map_points2();
+    // }
+
+    get calculate_team1() {
+        var ans = 0;
         for (var hero in self.team2.heroes) {
             for (var counter in self.team1.heroes) {
                 if (hero.is_counter(counter)) {
@@ -33,9 +37,10 @@ var Game = __class__ ('Game', [object], {
             }
         }
         return ans;
-    });},
-    get calculate_team2 () {return __get__ (this, function (self) {
-        ans = int (0);
+    }
+
+    get calculate_team2() {
+        var ans = 0;
         for (var hero in self.team1.heroes) {
             for (var counter in self.team2.heroes) {
                 if (hero.is_counter(counter)) {
@@ -44,9 +49,10 @@ var Game = __class__ ('Game', [object], {
             }
         }
         return ans;
-    });},
-    get calculate_map_points1 () {return __get__ (this, function (self) {
-        ans = int (0);
+    }
+
+    get calculate_map_points1() {
+        var ans = 0;
         for (var hero in self.the_map.heroes) {
             for (var each in self.team1.heroes) {
                 if (hero.has_advantage(each)) {
@@ -55,9 +61,10 @@ var Game = __class__ ('Game', [object], {
             }
         }
         return ans;
-    });},
-    get calculate_map_points2 () {return __get__ (this, function (self) {
-        ans = int (0);
+    }
+
+    get calculate_map_points2() {
+        var ans = 0;
         for (var hero in self.the_map.heroes) {
             for (var each in self.team2.heroes) {
                 if (hero.has_advantage(each)) {
@@ -66,18 +73,21 @@ var Game = __class__ ('Game', [object], {
             }
         }
         return ans;
-    });},
-    get __str__ () {return __get__ (this, function (self) {
-        ans = "///////TEAM 1///////\n";
-        ans += str (self.team1); //also not sure how to do str()
+    }
+    
+    toString() {
+        var ans = "///////TEAM 1///////\n";
+        ans += self.team1.toString(); //also not sure how to do str()
         ans += "\n///////TEAM 2///////\n";
-        ans += str (self.team2);
+        ans += self.team2.toString();
         return ans;
-    });},
-    get get_team1 () {return __get__ (this, function (self) {
+    }
+    
+    get get_team1() {
         return self.team1;
-    });},
-    get get_team2 () {return __get__ (this, function (self) {
+    }
+    
+    get get_team2() {
         return self.team2;
-    });}
-});
+    }
+}
--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 // import { Heroes } from './Hero.js'; uncomment once this is working
+import { Game } from './Game.js';
 window.onload = main();
 function main() {
     console.log("Main Function Started");
+    var game = new Game('team1', 'team2', 'map');
     // var hereos = Heroes();
-    // console.log(hereos.__str__());
+    console.log(game.toString());
 }
